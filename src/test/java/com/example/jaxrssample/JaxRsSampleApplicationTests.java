71fd478bbfe352dcfe7fb9f603e2737912e25ce9
pony-rider <peshne.vyacheslav@gmail.com>
2020-03-30 01:56:49 +0400

    init

---
+++ src/test/java/com/example/jaxrssample/JaxRsSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jaxrssample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JaxRsSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
